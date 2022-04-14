4e72900bf9d31a0d5c66aad5e49e41bb47c10cd0
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2022-04-14 18:11:10 +0200

    cleanup

--- index.js
+++ index.js
@@ -6,32 +6,34 @@ const port = process.env.PORT || '5000'
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.get('/', (req, res) => {
-    if (!req.headers.user || !req.headers.password) {
-        res.send("Missing Header");
-    } else { 
 
+
+app.get('/', async function (req, res) {
+
+    var bal = 'error';
+
+    if (req.headers.user && req.headers.password) {
         const credentials = {
             Username: req.headers.user,
             Password: req.headers.password
         }
 
-        fetch('http://portal.microdeb.se:44396/api/v1/login/194699f3-1f9e-47a4-8052-df26df0bc114/mps', {
+        await fetch('https://portal.microdeb.se:44396/api/v1/login/194699f3-1f9e-47a4-8052-df26df0bc114/mps', {
             body: JSON.stringify(credentials),
             method: 'POST',
             headers: { 'Content-Type': 'application/json' }
-
     })
             .then(response => response.json())
                 .then(data => {
-                    if (data[0]?.error) {
-                        res.send("Wrong Credentials")
-                    } else {
-                        const bal = JSON.stringify(data?.information.balance);
-                        res.send({bal: [bal.slice(0, bal.length-2), '.', bal.slice(bal.length-2)].join('')})
+                    if (!data[0]?.error) {
+                        const temp = JSON.stringify(data?.information.balance);
+                        bal = [temp.slice(0, temp.length-2), '.', temp.slice(temp.length-2)].join('');
                     }
                 });         
     }    
+
+    res.send({balance: bal})
+
   })
 
 app.listen(port, () => {
